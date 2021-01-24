1a6033dcedf1d7613fb78e54d8720c0e71712c6d
weldnor <kolesov.anton.s@gmail.com>
2021-01-25 02:14:47 +0300

    Add chat component template

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomePage } from './pages/home/home.page';
-import { HomeRoutingModule } from './home-routing-module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomePage} from './pages/home/home.page';
+import {HomeRoutingModule} from './home-routing-module';
+import {ChatComponent} from './components/chat/chat.component';
 
 
 @NgModule({
-  declarations: [HomePage],
+  declarations: [HomePage, ChatComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
