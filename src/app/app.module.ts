33ddd4699adbabdb3a27a3b430c4c83e55b11de3
weldnor <kolesov.anton.s@gmail.com>
2021-02-06 03:33:22 +0300

    Add shared module, improve recommendation-chat ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routiong-module';
 import {HttpClientModule} from '@angular/common/http';
-import {NbLayoutModule, NbThemeModule} from '@nebular/theme';
+import {NbIconModule, NbLayoutModule, NbThemeModule} from '@nebular/theme';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NbEvaIconsModule} from '@nebular/eva-icons';
+import {MainModule} from './modules/main/main.module';
+import {SharedModule} from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,6 +22,9 @@ import {NbEvaIconsModule} from '@nebular/eva-icons';
     NbThemeModule.forRoot({name: 'default'}),
     NbLayoutModule,
     NbEvaIconsModule,
+    MainModule,
+    SharedModule,
+    NbIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,16 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomePage} from './pages/home/home.page';
 import {HomeRoutingModule} from './home-routing-module';
-import {NbChatModule} from '@nebular/theme';
-import {RecommendationChatComponent} from './components/recommendation-chat/recommendation-chat.component';
 
 
 @NgModule({
-  declarations: [HomePage, RecommendationChatComponent],
+  declarations: [HomePage],
+  exports: [],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    NbChatModule
   ]
 })
 export class MainModule {
